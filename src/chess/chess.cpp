57a01c847207a9dbf710de85290b99f54f7e29e0
Daniil Shpilev <d_shpilev@mail.ru>
2022-03-13 18:29:21 +0700

    Create chess.cpp

---
+++ src/chess/chess.cpp
@@ -0,0 +1,21 @@
+#include "libchessviz/chessviz.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    char chess_board[BOARD_SIZE][BOARD_SIZE]
+            = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+               {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+               {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+
+    system("clear");
+    print_board(chess_board);
+    return 0;
+}
+
