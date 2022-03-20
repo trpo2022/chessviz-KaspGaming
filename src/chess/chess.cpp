4d8c05e1093a9149e959ba83c405aae7ce938e57
Daniil Shpilev <d_shpilev@mail.ru>
2022-03-20 12:56:01 +0700

    ignore bin/chess

--- src/chess/chess.cpp
+++ src/chess/chess.cpp
@@ -15,7 +15,21 @@ int main()
                {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
     system("clear");
+    motion motion;
+    cout << "Добро пожаловть в шахматы! Пока что реализовано движение только пешек" << endl;
+    cout << "Для выхода из программы ведите в терминал что-нибудь не относящееся к движению пешек\n" << endl;
     print_board(chess_board);
+    string step;
+    float number_of_turn = 1;
+    bool move_white = true; 
+    while (true) {
+	cout << (int)number_of_turn << ". ";
+	number_of_turn += 0.5;
+    	cin >> step;
+    	if (turn (step, motion, chess_board, move_white) != 0) return 1;
+	print_board(chess_board);
+	cout << endl;
+    }
     return 0;
 }
 
